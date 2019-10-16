6a1f6c1e8a1e5053388b4b62ad6b755ae7c88a40
BuildTools <unconfigured@null.spigotmc.org>
2019-10-15 23:53:12 -0400

    Figured out the threading stuff I basically had to overhaul the program twice but found out how to make it work. I will explain in class.

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,120 @@
 import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
 
-	public static CyclicBarrier phaseBarrier;
+	private static SortThread[] sortingThreads;
+	static int[] numberArray;
 
+	private static int numOfThreads;
+	private static int numOfPairs = 3;
+	private static int arraySize = 12;
 
 	public static void main(String[] args) {
+		if(arraySize % 2 != 0){
+			arraySize++;
+		}
 
+		numOfThreads = arraySize / (2 * numOfPairs);
 
+		System.out.printf("Size of Number Set: %d%n", arraySize);
+		System.out.printf("Number of Threads: %d%n", numOfThreads);
+		System.out.printf("Pairs per Thread: %d%n", numOfPairs);
+
+		//Generate array to sort
+		populateNumArray();
+
+		//Print the array before sorting
+		System.out.print("\nBefore Sort: ");
+		printArray(numberArray);
+		//PInitialize the thread array
+		sortingThreads = new SortThread[numOfThreads];
+
+		//Initialize the CyclicBarriers used for synchronization
+		CyclicBarrier phaseBarrier = new CyclicBarrier(numOfThreads);
+
+		//Populate the thread array
+		for (int i = 0; i < sortingThreads.length; i++) {
+			sortingThreads[i] = new SortThread(phaseBarrier, numberArray, 0, numOfPairs);
+			sortingThreads[i].setName("SortThread #" + (i+1));
+		}
+
+		long startTime = System.nanoTime();
+
+		try{
+			boolean isSorted = false;
+			//Loop until completely sorted
+			while (!isSorted) {
+				isSorted = true;
+
+				//Odd Phase
+				for (int i = 0; i < sortingThreads.length; i++) {
+					//Set starting index to odd phase index
+					sortingThreads[i] = new SortThread(phaseBarrier, numberArray, i * (numOfPairs * 2), numOfPairs);
+					sortingThreads[i].setName("SortThread #" + (i+1));
+					sortingThreads[i].start();
+				}
+				for (SortThread sortingThread : sortingThreads) {
+					sortingThread.join();
+				}
+
+				//Even Phase
+				int startIndex = 1;
+				for (int i = 0; i < sortingThreads.length; i++) {
+					//Set starting index to even phase index
+					sortingThreads[i] = new SortThread(phaseBarrier, numberArray, startIndex, numOfPairs);
+					sortingThreads[i].setName("SortThread #" + (i+1));
+					startIndex += 2 * numOfPairs;
+					sortingThreads[i].start();
+				}
+				for (SortThread sortingThread : sortingThreads) {
+					sortingThread.join();
+				}
+
+				//Check if completely sorted
+				for (SortThread sortingThread : sortingThreads) {
+					if (!sortingThread.isSorted()) {
+						isSorted = false;
+						break;
+					}
+				}
+			}
+		}catch (InterruptedException ignored){ }
+
+		long duration = System.nanoTime() - startTime;
+
+		//Print the array after sorting
+		System.out.print("After Sort: ");
+		printArray(numberArray);
+
+		System.out.printf("%nDuration: %f seconds", duration / 10e8);
+
+	}
+
+	private static void populateNumArray() {
+		//Random number generator
 		Random rand = new Random();
 
-		int[] array = new int[30];  //creates an array of numbers to sort
-		for (int i = 0; i < array.length; i++) {
-			array[i] = rand.nextInt();
+		numberArray = new int[arraySize];  //creates an array of numbers to sort
+		for (int i = 0; i < numberArray.length; i++) {
+			//Put some bounds on the ints to make it slightly more predictable
+			numberArray[i] = rand.nextInt(100) + 1;
+		}
+	}
+
+	public static void printArray(int[] array){
+		StringBuilder sb = new StringBuilder("[").append(array[0]);
+
+		for (int i = 1; i < array.length; i++){
+			sb.append(", ").append(array[i]);
 		}
 
-
-		SortThread[] threadArray = new SortThread[4];  //creates threads
-		int startIndex = 0;
-		for (int i = 0; i < threadArray.length; i++) {
-			threadArray[i] = new SortThread(array, startIndex, (array.length/2) / threadArray.length);
-		}
-
-		phaseBarrier = new CyclicBarrier(threadArray.length);
-
-		//Sets names of each thread and starts threads
-		for (int i = 0; i < threadArray.length; i++) {
-			threadArray[i].setName((i+1) + "");
-			threadArray[i].start();
-		}
-
-		//Joins threads
-		try {
-			for (int i = 0; i < threadArray.length; i++) {
-				threadArray[i].join();
-			}
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-		
-
+		sb.append("]");
+		System.out.println(sb.toString());
 	}
 
 
--- src/SortThread.java
+++ src/SortThread.java
@@ -1,81 +1,62 @@
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 public class SortThread extends Thread{
 
-    public int[] numbers;
-    public int[] sortedPairs;
-    public int numberOfPairs;
+	private static ThreadGroup sorting = new ThreadGroup("Sorting");
 
-    private int startIndex;
-    private int endIndex;
-    private boolean sorted;
+	private CyclicBarrier phaseBarrier;
+    private int[] numbers;
+    private int startIndex, endIndex;
+    private boolean isSorted;
 
-    public SortThread(int[] numberArray, int startIndex, int numOfPairs) {
-        this.numbers = numberArray;
-        this.sortedPairs = new int[numOfPairs*2];
-        this.numberOfPairs = numOfPairs;
+    public SortThread(CyclicBarrier barrier, int[] numberArray, int startIndex, int numOfPairs) {
+        super(sorting, "SortingThread");
+    	this.numbers = numberArray;
 		this.startIndex = startIndex;
-        this.endIndex = (startIndex + (numberOfPairs * 2));
+        this.endIndex = (startIndex + (numOfPairs * 2));
+        this.phaseBarrier = barrier;
+        isSorted = false;
     }
 
-    public void sortPairs(int startIndex) {
-        for (int i = 0; i < numberOfPairs; i++) {
-            if (numbers[startIndex-1] > numbers[startIndex]) {  //if the first number is larger
-                sortedPairs[startIndex-1] = numbers[startIndex];
-                sortedPairs[startIndex] = numbers[startIndex-1];
-            } else {
-                sortedPairs[startIndex-1] = numbers[startIndex-1];
-                sortedPairs[startIndex] = numbers[startIndex];
-            }
-            startIndex += 2;
-        }
-    }
+	@Override
+	public synchronized void start() {
+    	isSorted = false;
+		super.start();
+	}
 
-    public synchronized void start() {
-    	sorted = false;
-        super.start();
-    }
+	public void run() {
+		isSorted = true;
+		int temp;
 
-    public void run() {
-		while (!sorted){
-
-			boolean sorted = true;
-			int temp = 0;
-
-			//Perform Bubble Sort (Odd Pass)
-			for (int i = startIndex; i < endIndex && i < numbers.length - 1; i += 2){
-				if(numbers[i] > numbers[i+1]){
-					temp = numbers[i];
-					numbers[i] = numbers[i+1];
-					numbers[i+1] = temp;
-					sorted = false;
-				}
+		//Perform Bubble Sort
+		for (int i = startIndex; i < endIndex && i < numbers.length - 1; i += 2){
+			if(numbers[i] > numbers[i+1]){
+				temp = numbers[i];
+				numbers[i] = numbers[i+1];
+				numbers[i+1] = temp;
+				isSorted = false;
 			}
-
-			//Use CyclicBarrier to synchronize phases
-			//Wait until all threads have reached the even phase
-			try {
-				Main.phaseBarrier.await();
-			} catch (InterruptedException | BrokenBarrierException e) {
-				e.printStackTrace();
-			}
-
-			for (int i = startIndex + 1; i < endIndex && i < numbers.length - 1; i += 2){
-				if(numbers[i] > numbers[i+1]){
-					temp = numbers[i];
-					numbers[i] = numbers[i+1];
-					numbers[i+1] = temp;
-					sorted = false;
-				}
-			}
-
-			//Wait until all threads have completed the even phase
-			try {
-				Main.phaseBarrier.await();
-			} catch (InterruptedException | BrokenBarrierException e) {
-				e.printStackTrace();
-			}
-
 		}
+
+		try {
+			phaseBarrier.await();
+		} catch (InterruptedException | BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+
     }
+
+    public synchronized int getStartIndex(){
+    	return this.startIndex;
+    }
+
+    public void setStartIndex(int index){
+    	this.startIndex = index;
+    }
+
+    public synchronized boolean isSorted(){
+    	return this.isSorted;
+    }
+
 }
